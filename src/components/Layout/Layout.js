b2b3004f3ca77730599beccb7cab98f27b9bcce5
Ali Maihoob <alimaihoob.grand@outlook.com>
2022-06-08 14:22:29 +0400

    Add Toggle Button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Layout.css';
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = (props) => {
-    return (
-        <>
-            <Toolbar />
-            <SideDrawer />
-            <main className={classes.Content}>
-                { props.children }
-            </main>
-        </>
-    );
-};
+class Layout extends Component{
+    state = {
+        showSide: true
+    }
+
+    closeSidebarHandler = () => {
+        this.setState({
+            showSide: false
+        })
+    }
+
+    toggleSidebar = () => {
+        this.setState((prevState) => {
+            return {
+                showSide: !prevState.showSide
+            }
+        })
+    }
+
+    render () {
+        return (
+            <>
+                <Toolbar toggleSidebar={this.toggleSidebar}/>
+                <SideDrawer
+                    open={this.state.showSide}
+                    closed={this.closeSidebarHandler}
+                />
+                <main className={classes.Content}>
+                    { this.props.children }
+                </main>
+            </>
+        );
+    }
+}
 export default Layout;
